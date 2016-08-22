6950295b79474ab8077e563a8ab5e81b90c0d4a6
Teemu Koivisto <teemu@Teemu-MacBook-Air.local>
2016-08-23 00:22:20 +0300

    Add error handling to gulp scripts with gulp-plumber

--- gulp/build-app.js
+++ gulp/build-app.js
@@ -4,6 +4,7 @@ const annotate = require("gulp-ng-annotate");
 const concat = require("gulp-concat");
 const uglify = require("gulp-uglify");
 const sourcemaps = require("gulp-sourcemaps");
+const plumber = require("gulp-plumber");
 const livereload = require("gulp-livereload");
 
 gulp.task("clean:dist", function () {
@@ -16,6 +17,7 @@ gulp.task("build", ["clean:dist"], () => {
 
 gulp.task("build-js", () => {
   return gulp.src("./src/app/**/*.js")
+    .pipe(plumber())
     .pipe(concat("app.min.js"))
     .pipe(annotate())
     .pipe(sourcemaps.init())
@@ -26,6 +28,7 @@ gulp.task("build-js", () => {
 
 gulp.task("build-js-min", () => {
   return gulp.src("./src/app/**/*.js")
+    .pipe(plumber())
     .pipe(concat("app.min.js"))
     .pipe(annotate())
     .pipe(sourcemaps.init())
@@ -37,6 +40,7 @@ gulp.task("build-js-min", () => {
 
 gulp.task("build-css", () => {
   return gulp.src("./src/app/**/*.css")
+    .pipe(plumber())
     .pipe(concat("styles.css"))
     .pipe(sourcemaps.init())
     .pipe(sourcemaps.write("."))
@@ -46,12 +50,14 @@ gulp.task("build-css", () => {
 
 gulp.task("build-html", () => {
   return gulp.src("./src/app/components/**/*.html")
+    .pipe(plumber())
     .pipe(gulp.dest("./dist/templates"))
     .pipe(livereload());
 });
 
 gulp.task("build-bower", () => {
   return gulp.src(["./src/bower_components/**/*.min.*", "./src/bower_components/**/fonts/*"])
+    .pipe(plumber())
     .pipe(gulp.dest("./dist/bower"))
     .pipe(livereload());
 });
