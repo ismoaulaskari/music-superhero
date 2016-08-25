af375331209e79301bd27c6ae5b369cb811d5749
Ismo Aulaskari <ismo@aulaskari.com>
2016-08-25 21:56:06 +0300

    comment

--- src/app/components/record/log.decorator.js
+++ src/app/components/record/log.decorator.js
@@ -2,7 +2,9 @@ MyApp.config(function($provide) {
   $provide.decorator('$log', function($delegate, $sniffer, NoteService) {
         var _log = $delegate.log; //Saving the original behavior
 
+        //we want to log to a service instead of just the console
         $delegate.log = function(message) { NoteService.newNote(message) };
+                
 //        $delegate.error = function(message) {
 //            alert(message);
 //        }
