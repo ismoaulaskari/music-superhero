7b7efe6ff4ef8c2dd6009c1ba3007a2137b186c1
Ismo Aulaskari <ismo@aulaskari.com>
2016-08-28 10:59:36 +0300

    piano sound

--- src/app/components/record/record.service.js
+++ src/app/components/record/record.service.js
@@ -29,7 +29,7 @@ MyApp.service("RecordService", ["$log", function ($log) {
       $log.log({pitch: self.tuner.pitch, note: self.tuner.noteName});
       if (self.running) {
         requestAnimationFrame(self.logPitch);
-      }      
+      }
     };
     // logPitch();
 // If you sing into your microphone, your pitch will be logged to the console in real time.
@@ -39,23 +39,76 @@ MyApp.service("RecordService", ["$log", function ($log) {
       self.voice.stop();
     };
 
-    self.play = function (song) {      
-      console.log("play")      
-      var saw = new Wad({source : 'sawtooth'})
+    self.play = function (song) {
+      console.log("play")
+      var player = makePiano();
       for (var i = 0; i < song.length; i++) {
-        var note = song[i];        
+        var note = song[i];
         if (typeof note.note !== "undefined" && note.length > 0) {
           console.log("playing" + note.note + " for " + note.length);
-          saw.play({
+          player.play({
             volume: 0.5,
-       //     wait: 0, // Time in seconds between calling play() and actually triggering the note.
-         //   loop: false, // This overrides the value for loop on the constructor, if it was set.
+            
+            
+            //wait: 0.5, // Time in seconds between calling play() and actually triggering the note.
+            //   loop: false, // This overrides the value for loop on the constructor, if it was set.
             pitch: note.note, // A4 is 440 hertz.
             label: note.note // A label that identifies this note.         
           });
         }
       }
-      saw.stop();
+      player.stop();
     };
+
+    var makePiano = function () {
+      return new Wad({
+        source: 'square',
+        env: {
+          attack: .01,
+          decay: .005,
+          sustain: 1,
+          hold: .5,
+          release: .3
+        },
+        filter: {
+          type: 'lowpass',
+          frequency: 1200,
+          q: 8.5,
+          env: {
+            attack: .2,
+            frequency: 600
+          }
+        }
+      })
+
+      var makeFlute = function () {
+        return new Wad({
+          source: 'square',
+          env: {
+            attack: .015,
+            decay: .002,
+            sustain: .5,
+            hold: 2.5,
+            release: .3
+          },
+          filter: {
+            type: 'lowpass',
+            frequency: 600,
+            q: 7,
+            env: {
+              attack: .7,
+              frequency: 1600
+            }
+          },
+          vibrato: {
+            attack: 8,
+            speed: 8,
+            magnitude: 100
+          }
+        })
+
+      }
+
+    }
   }]);
 
