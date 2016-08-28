28d020336ee4d0e0d8594313085babd1b0119762
Ismo Aulaskari <ismo@aulaskari.com>
2016-08-28 09:15:31 +0300

    changed vexflow from Easyscore to standard notation and now notes can be seen

--- src/app/components/playback/note.service.js
+++ src/app/components/playback/note.service.js
@@ -28,8 +28,8 @@ MyApp.service("NoteService", [function () {
 
     //$bpm = 60; $bpm <= 179; a minute is 60 * 1000 ms., a qarter note is 1000ms in 60bpm in 4/4 
     var toVexflowNoteLength = function (millisec, bpm) {
-      var preF = "/";
-      var postF = ", ";
+      var preF = "";
+      var postF = "";
       if (millisec > 0 && bpm > 0) {
         if (millisec / bpm > 60)
           return preF + "w" + postF;
@@ -46,27 +46,31 @@ MyApp.service("NoteService", [function () {
       return preF + "16" + postF;
     };
 
-    self.convertToVexflowNotes = function (song, bpm) {
-      var notes = "";
+    self.convertToVexflowNotes = function (song, bpm) {   
+      var OCTAVE_SEPARATOR = '/';
+      var notes = [];
       var currentNote;
       var currentTime = 0;
       for (var i = 0; i < song.length; i++) {
         var note = song[i];
-        console.log(note);
+        //console.log(note);
         //console.log("note=" + note.note + " len=" + note.length);
         if (typeof note.note !== undefined && note.length >= 0) {
           if (currentNote !== note.note) { //pitch changed
             if (typeof currentNote == "undefined") {
               currentNote = "b4";
             }
-            notes = notes + currentNote + toVexflowNoteLength(currentTime, bpm);
+            var noteNameLen = currentNote.length-1;
+            currentNote = currentNote.slice(0, noteNameLen) + OCTAVE_SEPARATOR + currentNote.slice(noteNameLen);
+            console.log("curnote: " + currentNote);
+            notes.push(new VF.StaveNote({keys: [currentNote], duration: toVexflowNoteLength(currentTime, bpm)}));
             currentNote = note.note;
             currentTime = note.length;
           } else { //pitch continues
             currentTime = currentTime + note.length;
           }
         } else { //no note detected
-      //    notes = notes + "b4" + "/qr, "; //rest 
+          //    notes = notes + "b4" + "/qr, "; //rest 
         }
       }
       return notes;
--- src/app/components/playback/playback.controller.js
+++ src/app/components/playback/playback.controller.js
@@ -7,7 +7,7 @@ MyApp.controller("PlaybackController", ["$scope", "NoteService", "RecordService"
       RecordService.play(NoteService.getSong());
     };
 
-    $scope.noteGen = function () {
+    $scope.noteGenOld = function () {
 //      console.log("Making sheet");
 
       var vf = new Vex.Flow.Factory({
@@ -21,12 +21,50 @@ MyApp.controller("PlaybackController", ["$scope", "NoteService", "RecordService"
       console.log("notes is " + notes);
       system.addStave({
         voices: [
-                    score.voice(score.notes(notes, {stem: "up"})),
+          score.voice(score.notes(notes, {stem: "up"}), {strict: false}),
 //          score.voice(score.notes(notes, {stem: "up"})),
-                 //  score.voice(score.notes("C#4/h, C#4", { stem: "down" })),
+                  //  score.voice(score.notes("C#4/h, C#4", { stem: "down" })),
         ],
-      }).addClef("treble").addTimeSignature("4/4");
+      });//.addClef("treble").addTimeSignature("4/4");
+
 
       vf.draw();
     };
+
+    $scope.noteGen = function () {
+      VF = Vex.Flow;
+      var notes = NoteService.convertToVexflowNotes(NoteService.getSong(), 60);
+      console.log("notes is " + notes);
+
+// Create an SVG renderer and attach it to the DIV element named "boo".
+      var div = document.getElementById("boo")
+      var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+
+// Configure the rendering context.
+      renderer.resize(500, 200);
+      var context = renderer.getContext();
+      context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+
+// Create a stave of width 400 at position 10, 40 on the canvas.
+      var stave = new VF.Stave(10, 40, 400);
+
+// Add a clef and time signature.
+      stave.addClef("treble").addTimeSignature("4/4");
+
+// Connect it to the rendering context and draw!
+      stave.setContext(context).draw();
+
+      // Create a voice in 4/4 and add above notes
+      var voice = new VF.Voice({num_beats: 4, beat_value: 4});
+      voice.setStrict(false);
+      voice.addTickables(notes);
+
+// Format and justify the notes to 400 pixels.
+      var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
+
+// Render voice
+      voice.draw(context, stave);
+
+    };
+
   }]);
